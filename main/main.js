45a4cea07f0bb99f682d4d622a1dfd82236f948a
xiaoming <15xmchen@stu.edu.cn>
2019-05-25 20:05:47 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
 // Write your cade below:
-module.exports = function main() {
-  
+module.exports = function main(num1, num2) {
+    // Using try-catch to avoid some errors such as the num2 equals to zero, 
+    // num1 or num2 is not the numeric type, num1 or num2 is overflow and so on.
+    try {
+        return (num1 % num2);
+    } catch (error) {
+        console.log("error!");
+    }
 };
